2f92716d919d6ece26b83b725ca1ddd64364f3a7
Andrew Zhidenko <andrei.zhidenko@kandasoft.com>
2024-12-02 15:51:41 +0300

    Day2 refactored.

---
+++ AdventOfCode2024/AdventOfCode2024/Helpers/ParsingHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Helpers
+{
+    public static class ParsingHelper
+    {
+        public static List<int> ConvertStringToIntList(string line, string separator)
+        {
+            var items = line.Split(' ').Where(x => string.IsNullOrEmpty(x) == false);
+            return items.Select(int.Parse).ToList();
+        }
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode2024/Tasks/Task2.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode2024.Helpers;
+
+namespace AdventOfCode2024.Tasks
+{
+    public class Task2
+    {
+        List<List<int>> _reportList = new List<List<int>>();
+
+        public Task2()
+        {
+            var lines = FileHelper.ReadLines("Input2.txt");
+
+            foreach (var line in lines)
+                _reportList.Add(ParsingHelper.ConvertStringToIntList(line, " "));
+        }
+
+        public void Part1()
+        {
+            var saveCount = 0;
+
+            foreach (var report in _reportList)
+                if (IsReportSafe(report))
+                    saveCount++;
+
+            OutputHelper.ShowResult(2, 1, saveCount);
+        }
+
+        public void Part2()
+        {
+            var saveCount = 0;
+
+            foreach (var report in _reportList)
+            {
+                if (IsReportSafe(report))
+                {
+                    saveCount++;
+                    continue;
+                }
+
+                for (int i = 0; i < report.Count; i++)
+                {
+                    var newReport = report.ToList();
+                    newReport.RemoveAt(i);
+
+                    if (IsReportSafe(newReport))
+                    {
+                        saveCount++;
+                        break;
+                    }
+                }
+            }
+
+            OutputHelper.ShowResult(2, 2, saveCount);
+        }
+
+        private bool IsReportSafe(List<int> report)
+        {
+            var increasingFlag = report[0] < report[1];
+
+            for (int i = 0; i < report.Count - 1; i++)
+            {
+                var diff = Math.Abs(report[i] - report[i + 1]);
+
+                if (diff < 1 || diff > 3)
+                    return false;
+
+                if ((increasingFlag && report[i] >= report[i + 1]) || (!increasingFlag && report[i] <= report[i + 1]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
