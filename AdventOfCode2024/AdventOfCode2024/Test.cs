d5459ed137eae87a974607b93b5c7dcdb031fdee
Andrew Zhidenko <a.zhidenko@softteco.com>
2026-04-17 00:57:23 +0300

    test.

--- AdventOfCode2024/AdventOfCode2024/Test.cs
+++ AdventOfCode2024/AdventOfCode2024/Test.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,35 @@ namespace AdventOfCode2024
 
             // Snyk flags this as Command Injection
             Process.Start("bash", "-c \"" + input + "\"");
+        }
+
+        public void Run2()
+        {
+            string password = "test123";
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        public void Run3()
+        {
+            string filename = Environment.GetEnvironmentVariable("FILE_NAME");
+            string path = "C:\\temp\\" + filename;
+
+            File.WriteAllText(path, "test");
+        }
+
+        public void Run4()
+        {
+            string userInput = Environment.GetEnvironmentVariable("USER_INPUT");
+
+            string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'";
+
+            using (var conn = new SqlConnection("Server=.;Database=Test;Trusted_Connection=True;"))
+            {
+                conn.Open();
+                var cmd = new SqlCommand(query, conn);
+                cmd.ExecuteReader();
+            }
         }
     }
 }
